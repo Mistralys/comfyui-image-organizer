f6f56187509ffeffa7d7ebd7a6a75ca3dcc6784a
Mistralys <s.mordziol@gmail.com>
2025-07-26 20:05:10 +0200

    Added post-autoload class cache clearing.

--- src/ComfyUIOrganizer/Builder.php
+++ src/ComfyUIOrganizer/Builder.php
@@ -4,16 +4,39 @@ declare(strict_types=1);
 
 namespace Mistralys\ComfyUIOrganizer;
 
+use AppUtils\ClassHelper;
+use AppUtils\ClassHelper\Repository\ClassRepository;
+use Mistralys\X4\UI\Console;
 use const Mistralys\ComfyUIOrganizer\Config\APP_IMAGE_FOLDER;
 
 class Builder
 {
-    public static function build() : void
+    public static function build(): void
     {
-        require_once __DIR__.'/../../vendor/autoload.php';
-        require_once __DIR__.'/../../config.php';
+        self::init();
 
-        $app = new OrganizerApp(APP_IMAGE_FOLDER);
-        new ImageIndexer($app)->indexImages();
+        new ImageIndexer(OrganizerApp::create())->indexImages();
+    }
+
+    public static function postAutoload(): void
+    {
+        self::init();
+
+        Console::line1('Clearing class cache.');
+
+        ClassHelper::getRepositoryManager()->clearCache();
+    }
+
+    private static bool $initialized = false;
+
+    private static function init(): void
+    {
+        if (self::$initialized) {
+            return;
+        }
+
+        self::$initialized = true;
+
+        require_once __DIR__ . '/../../prepend.php';
     }
 }
