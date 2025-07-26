1b248d6518912f2c57b78f9fc3917b544c5841a2
Mistralys <s.mordziol@gmail.com>
2025-07-26 13:16:15 +0200

    Config template.

---
+++ config.dist.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\ComfyUIOrganizer\Config;
+
+const APP_WEBROOT_URL = 'http://127.0.0.1/comfyui-image-organizer';
+const APP_IMAGE_FOLDER = 'C:\Users\Username\Documents\ComfyUI\output';
