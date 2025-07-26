6dabfcbcccac6405cd2565823c9bfb4a555408a7
Mistralys <s.mordziol@gmail.com>
2025-07-26 20:05:34 +0200

    Added the class helper cache.

--- prepend.php
+++ prepend.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use AppUtils\ClassHelper;
+use AppUtils\FileHelper\FolderInfo;
 use AppUtils\Request;
 use const Mistralys\ComfyUIOrganizer\Config\APP_WEBROOT_URL;
 
@@ -9,3 +11,5 @@ require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/config.php';
 
 Request::getInstance()->setBaseURL(APP_WEBROOT_URL);
+
+ClassHelper::setCacheFolder(FolderInfo::factory(__DIR__.'/cache')->create());
