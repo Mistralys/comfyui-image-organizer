b8778476ad22c5886a0123df05fb642cdca5ae2b
Mistralys <s.mordziol@gmail.com>
2025-07-28 22:24:16 +0200

    URL collection class.

---
+++ src/ComfyUIOrganizer/URLs.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\ComfyUIOrganizer;
+
+use Mistralys\ComfyUIOrganizer\Pages\ImageBrowser;
+use Mistralys\ComfyUIOrganizer\Pages\IndexManagerPage;
+use Mistralys\X4\UI\Page\BasePage;
+use Mistralys\X4\UI\UserInterface;
+use const Mistralys\ComfyUIOrganizer\Config\APP_WEBROOT_URL;
+
+class URLs
+{
+    private UserInterface $ui;
+
+    public function __construct(UserInterface $ui)
+    {
+        $this->ui = $ui;
+    }
+
+    public function browser() : string
+    {
+        return $this->build(ImageBrowser::URL_NAME);
+    }
+
+    public function indexManager(array $params=array()) : string
+    {
+        return $this->build(IndexManagerPage::URL_NAME, $params);
+    }
+
+    private function build(string $pageName, array $params=array()) : string
+    {
+        $params[BasePage::REQUEST_PARAM_PAGE] = $pageName;
+
+        return $this->ui->getRequest()->setBaseURL(APP_WEBROOT_URL)->buildURL($params);
+    }
+}
