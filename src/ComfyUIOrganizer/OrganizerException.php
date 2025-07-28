e64e221d62a9c97b767c6ec4e124122ef08cae44
Mistralys <s.mordziol@gmail.com>
2025-07-28 17:47:06 +0200

    Added an error constant.

--- src/ComfyUIOrganizer/OrganizerException.php
+++ src/ComfyUIOrganizer/OrganizerException.php
@@ -9,4 +9,5 @@ use AppUtils\BaseException;
 class OrganizerException extends BaseException
 {
     public const int ERROR_CANNOT_SAVE_INEXISTENT_IMAGE = 179401;
+    public const int ERROR_CANNOT_MOVE_FILE_EXISTS = 179402;
 }
