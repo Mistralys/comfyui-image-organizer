d6edb3161271857fd6f6e4ab87c5adad31b1280c
Mistralys <s.mordziol@gmail.com>
2025-07-28 22:23:35 +0200

    Added a base page to handle navigation items.

---
+++ src/ComfyUIOrganizer/BaseOrganizerPage.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\ComfyUIOrganizer;
+
+use Mistralys\X4\UI\Icon;
+use Mistralys\X4\UI\Page\BasePage;
+use Mistralys\X4\UI\Page\NavItem;
+use function AppLocalize\t;
+
+abstract class BaseOrganizerPage extends BasePage
+{
+    public function getNavItems(): array
+    {
+        return array(
+            new NavItem(Icon::typeSolid('bars').' '.t('Browser'), OrganizerApp::create()->url()->browser()),
+            new NavItem(Icon::typeSolid('database').' '.t('Index manager'), OrganizerApp::create()->url()->indexManager()),
+        );
+    }
+}
