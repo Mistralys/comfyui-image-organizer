fc8a54537737730b9836d5d7192eb69f336b5cfb
Mistralys <s.mordziol@gmail.com>
2025-07-29 22:14:44 +0200

    Added session start.

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require_once __DIR__.'/prepend.php';
 use Mistralys\X4\UI\UserInterface;
 use const Mistralys\ComfyUIOrganizer\Config\APP_WEBROOT_URL;
 
+session_start();
+
 $app = OrganizerApp::create();
 
 $ui = new UserInterface(
