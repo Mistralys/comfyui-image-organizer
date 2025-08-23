feb72341a648e0fb524666773e5825ca4fcbb38d
Mistralys <s.mordziol@gmail.com>
2025-08-23 23:00:47 +0200

    Added getters...
    and moved the LoRA summary here too.

--- src/ComfyUIOrganizer/ImageProperties.php
+++ src/ComfyUIOrganizer/ImageProperties.php
@@ -5,16 +5,24 @@ declare(strict_types=1);
 namespace Mistralys\ComfyUIOrganizer;
 
 use AppUtils\ArrayDataCollection;
+use Mistralys\ComfyUIOrganizer\LoRAs\LoRAsCollection;
 
 class ImageProperties
 {
-    const string KEY_TEST_NAME = 'testName';
-    const string KEY_TEST_NUMBER = 'testNumber';
-    const string KEY_SEED = 'seed';
-    const string KEY_FOLDER_NAME = 'folder';
+    public const string KEY_TEST_NAME = 'testName';
+    public const string KEY_TEST_NUMBER = 'testNumber';
+    public const string KEY_SEED = 'seed';
+    public const string KEY_FOLDER_NAME = 'folder';
     public const string KEY_IMG_BATCH_NR = 'imgBatchNr';
     public const string KEY_FAVORITE = 'favorite';
     public const string KEY_UPSCALED_IMAGE = 'upscaledImage';
+    public const string KEY_CFG = 'imgCFG';
+    public const string KEY_PROMPT_NEGATIVE = 'promptNegative';
+    public const string KEY_PROMPT_POSITIVE = 'promptPositive';
+    public const string KEY_SAMPLER = 'sampler';
+    public const string KEY_SAMPLER_STEPS = 'samplerSteps';
+    public const string KEY_SCHEDULER = 'scheduler';
+    public const string KEY_FACEFIX = 'isFacefix';
 
     private ArrayDataCollection $data;
 
@@ -36,6 +44,36 @@ class ImageProperties
         return $this->data->getInt(self::KEY_SEED);
     }
 
+    public function getCFG() : float
+    {
+        return $this->data->getFloat(self::KEY_CFG);
+    }
+
+    public function getPromptNegative() : string
+    {
+        return $this->data->getString(self::KEY_PROMPT_NEGATIVE);
+    }
+
+    public function getPromptPositive() : string
+    {
+        return $this->data->getString(self::KEY_PROMPT_POSITIVE);
+    }
+
+    public function getSampler() : string
+    {
+        return $this->data->getString(self::KEY_SAMPLER);
+    }
+
+    public function getSamplerSteps() : int
+    {
+        return $this->data->getInt(self::KEY_SAMPLER_STEPS);
+    }
+
+    public function getScheduler() : string
+    {
+        return $this->data->getString(self::KEY_SCHEDULER);
+    }
+
     public function getFolderName() : string
     {
         return $this->data->getString(self::KEY_FOLDER_NAME);
@@ -49,6 +87,29 @@ class ImageProperties
     public function getBatchNumber() : int
     {
         return $this->data->getInt(self::KEY_IMG_BATCH_NR);
+    }
+
+    public function getLoRASummary() : string
+    {
+        $loraIDs = LoRAsCollection::getInstance()->getIDs();
+
+        $loras = array_filter(
+            $this->serialize(),
+            function ($key) use ($loraIDs) {
+                return in_array($key, $loraIDs);
+            },
+            ARRAY_FILTER_USE_KEY
+        );
+
+        ksort($loras);
+
+        $loraSummary = array();
+        foreach ($loras as $key => $value)
+        {
+            $loraSummary[] = $key.' '.str_replace('0.', '.', $value);
+        }
+
+        return implode(', ', $loraSummary);
     }
 
     public function getTestName() : string
@@ -128,6 +189,11 @@ class ImageProperties
     public function isFavorite() : bool
     {
         return $this->data->getBool(self::KEY_FAVORITE);
+    }
+
+    public function isFacefix() : bool
+    {
+        return $this->data->getBool(self::KEY_FACEFIX);
     }
 
     public function setFolderName(string $folderName) : self
