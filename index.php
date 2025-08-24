c09dd5a948e6c742d10f72157baa4fde661b53a9
Mistralys <s.mordziol@gmail.com>
2025-08-24 12:48:23 +0200

    Removed obsolete session start, fixed UI creation.

--- index.php
+++ index.php
@@ -6,19 +6,11 @@ namespace Mistralys\ComfyUIOrganizer;
 
 require_once __DIR__.'/prepend.php';
 
-use Mistralys\X4\UI\UserInterface;
 use const Mistralys\ComfyUIOrganizer\Config\APP_WEBROOT_URL;
 
-session_start();
-
-$app = OrganizerApp::create();
-
-$ui = new UserInterface(
-    $app,
-    APP_WEBROOT_URL,
-    APP_WEBROOT_URL.'/vendor'
-);
-
-$app->setUI($ui);
-
-$ui->display();
+OrganizerApp::create()
+    ->createUI(
+        APP_WEBROOT_URL,
+        APP_WEBROOT_URL.'/vendor'
+    )
+    ->display();
