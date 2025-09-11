7af4552a07c609565e82b6f5797335a3a94190cd
Mistralys <s.mordziol@gmail.com>
2025-09-11 19:32:25 +0200

    Added `workflows()` to the main URLs.

--- src/ComfyUIOrganizer/URLs.php
+++ src/ComfyUIOrganizer/URLs.php
@@ -6,6 +6,7 @@ namespace Mistralys\ComfyUIOrganizer;
 
 use Mistralys\ComfyUIOrganizer\Pages\ImageBrowser;
 use Mistralys\ComfyUIOrganizer\Pages\IndexManagerPage;
+use Mistralys\ComfyUIOrganizer\Pages\WorkflowsPage;
 use Mistralys\X4\UI\Page\BasePage;
 use Mistralys\X4\UI\UserInterface;
 use const Mistralys\ComfyUIOrganizer\Config\APP_WEBROOT_URL;
@@ -27,6 +28,11 @@ class URLs
     public function indexManager(array $params=array()) : string
     {
         return $this->build(IndexManagerPage::URL_NAME, $params);
+    }
+
+    public function workflows(array $params=array()) : string
+    {
+        return $this->build(WorkflowsPage::URL_NAME, $params);
     }
 
     private function build(string $pageName, array $params=array()) : string
