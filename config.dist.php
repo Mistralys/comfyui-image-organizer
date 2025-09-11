f91b3ac372ae1db844898ffb7afbd3c2cbe0605a
Mistralys <s.mordziol@gmail.com>
2025-09-11 07:24:18 +0200

    Added the ComfyUI installation folder constant.

--- config.dist.php
+++ config.dist.php
@@ -4,5 +4,17 @@ declare(strict_types=1);
 
 namespace Mistralys\ComfyUIOrganizer\Config;
 
+/**
+ * URL to the web root of this application in the local webserver.
+ */
 const APP_WEBROOT_URL = 'http://127.0.0.1/comfyui-image-organizer';
-const APP_IMAGE_FOLDER = 'C:\Users\Username\Documents\ComfyUI\output';
+
+/**
+ * Absolute path to the ComfyUI installation folder.
+ */
+const APP_COMFYUI_FOLDER = 'C:\Users\newsl\Documents\ComfyUI';
+
+/**
+ * Absolute path to the folder in which ComfyUI saves generated images.
+ */
+const APP_IMAGE_FOLDER = APP_COMFYUI_FOLDER.'\output';
